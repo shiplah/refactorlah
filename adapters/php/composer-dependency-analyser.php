c433f36ae211c1e974c73046b9f39cc523f1b021
NickSdot <catch.github.com.nicksdot@nicksdot.dev>
2026-05-21 11:19:18 +0700

    chore: add php dev tooling dependencies

--- adapters/php/composer-dependency-analyser.php
+++ adapters/php/composer-dependency-analyser.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
-use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 $config = new Configuration();
 
@@ -13,6 +12,4 @@ return $config
     ->addPathToScan(__DIR__ . '/tests', isDev: true)
     ->addPathToScan(__DIR__ . '/.php-cs-fixer.dist.php', isDev: true)
     ->addPathToScan(__DIR__ . '/rector.php', isDev: true)
-    ->ignoreErrorsOnPackage('phpstan/phpstan', [ErrorType::UNUSED_DEPENDENCY])
-    ->ignoreErrorsOnPackage('shipmonk/dead-code-detector', [ErrorType::UNUSED_DEPENDENCY])
 ;
