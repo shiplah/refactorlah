0f06bf396af999bf32aee1d789d6f9c017d14412
NickSdot <catch.github.com.nicksdot@nicksdot.dev>
2026-05-22 11:01:22 +0700

    chore: keep php dependency analysis clean

--- adapters/php/composer-dependency-analyser.php
+++ adapters/php/composer-dependency-analyser.php
@@ -10,6 +10,7 @@ return $config
     ->addPathToScan(__DIR__ . '/src', isDev: false)
     ->addPathToScan(__DIR__ . '/bin', isDev: false)
     ->addPathToScan(__DIR__ . '/tests', isDev: true)
+    ->addPathToExclude(__DIR__ . '/tests/fixtures')
     ->addPathToScan(__DIR__ . '/.php-cs-fixer.dist.php', isDev: true)
     ->addPathToScan(__DIR__ . '/rector.php', isDev: true)
 ;
